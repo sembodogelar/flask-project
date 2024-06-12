512b34983274e86386413cd9fa8601be2e54e396
SembodoGelar <sembodogelar6@gamail.com>
2024-06-12 10:15:39 +0700

    Add bootstrap as frontend framework

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, app = Flask(__name__, template_folder='../templates'))
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
